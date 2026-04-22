70e842b4f64415ade52a43f9d4cafe02c8d9e0f1
Kacper <kacper.piotrowski1711@gmail.com>
2026-04-22 22:18:35 +0200

    Add GetWithParams Endpoint

--- ClinicAPI/Controllers/AppointmentsController.cs
+++ ClinicAPI/Controllers/AppointmentsController.cs
@@ -1,3 +1,4 @@
+using ClinicAPI.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,7 +6,13 @@ namespace ClinicAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AppointmentsController : ControllerBase
+    public class AppointmentsController(IAppointmentService service) : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetWithParams([FromQuery] string? status,
+            [FromQuery] string? patientLastName, CancellationToken ct)
+        {
+            return Ok(await service.GetWithParamsAsync(status, patientLastName, ct));
+        }
     }
 }
